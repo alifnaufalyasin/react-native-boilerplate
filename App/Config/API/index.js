bad86c6232584d62841368a233a83588f431522d
Topik Mujianto <topikmujianto7@gmail.com>
2019-07-29 09:43:48 +0700

    Add files via upload

---
+++ App/Config/API/index.js
@@ -0,0 +1,4 @@
+export default class API {
+    static users = '/users/'
+}
+
---
+++ App/Config/BaseURL/index.js
@@ -0,0 +1,20 @@
+import API from '../API'
+
+const env = {
+    local: 'local',
+    dev: 'dev',
+    stage: 'stage',
+    production: 'production',
+  }
+  
+  const API_URL = {
+    local: '',
+    dev: '',
+    stage:'https://jsonplaceholder.typicode.com',
+    production: 'https://jsonplaceholder.typicode.com'
+  }
+  
+  const currentEnv = env.stage //change config here
+  export const BASE_URL = API_URL[currentEnv]
+  export const Api = API
+  
